74b458050e40c7f384576a58cf852d2ca99d309e
yanuashrf <yanuashrofi@gmail.com>
2024-06-06 12:06:34 +0700

    update icon dan penambahan menu all role

--- resources/views/admin/Dashboard.blade.php
+++ resources/views/admin/Dashboard.blade.php
@@ -31,12 +31,12 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Penyulang
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Penyulang
                                 </div>
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $countPenyulang }}</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                
                             </div>
                         </div>
                     </div>
@@ -91,13 +91,13 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah mVA
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah mVA
 
                                 </div>
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $mva }}</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                               
                             </div>
                         </div>
                     </div>
